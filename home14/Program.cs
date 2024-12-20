45d568c0b2557fdfde2784cb03da8fdd1d2f6df7
Abubakrj0n <big500530@gmail.com>
2024-12-20 16:28:53 +0500

    Add files via upload

---
+++ home14/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace home14
+{
+    class Animal
+    {
+        public string Name = "Sher";
+        public string Type = "Mard";
+        public int Age = 12;
+        
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Type: {Type}, Age: {Age}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+
+            Console.WriteLine($"Animal Name: {animal.Name}");
+            Console.WriteLine($"Animal Type: {animal.Type}");
+            Console.WriteLine($"Animal Age: {animal.Age}");
+
+            animal.PrintInfo();
+        }
+    }
+}
